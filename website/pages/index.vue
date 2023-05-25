56cd3a9e5527d22f8180e68092dc5bb57c96edf2
FrancescaGrimaldi <francesca1.grimaldi@mail.polimi.it>
2023-05-25 09:52:54 +0200

    modified index

--- website/pages/index.vue
+++ website/pages/index.vue
@@ -2,6 +2,9 @@
     <NuxtLink id="to_our_team" to="/our_team">Our Team</NuxtLink>
     <NuxtLink id="to_all_areas" to="/all_areas">All Areas</NuxtLink>
     <NuxtLink id="to_all_projects" to="/projects">All Projects</NuxtLink>
+    <NuxtLink id="to_contacts" to="/contacts">Contacts</NuxtLink>
+    <NuxtLink id="to_about_us" to="/about_us">About Us</NuxtLink>
+    <NuxtLink id="to_portfolio" to="/portfolio">Portfolio</NuxtLink>
 </template>
 
 <style>
