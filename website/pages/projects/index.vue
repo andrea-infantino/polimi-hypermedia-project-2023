b327117c4d602f64237f7ab3c555a71140c3f668
FrancescaGrimaldi <francesca1.grimaldi@mail.polimi.it>
2023-05-31 19:44:44 +0200

    improved all projects order

--- website/pages/projects/index.vue
+++ website/pages/projects/index.vue
@@ -5,34 +5,52 @@
           ALL PROJECTS
         </div>
       </div>
-      <div id="list-container">
-        <ProjectCard v-for = "project of Projects" :id = "project.id" :title  = "project.title" :link = "'/projects/' + project.id"/>
+
+      <div class="alphabetical-projects">
+        <div class="proj-left">
+          <div v-for="letter of letters1" class="letter-block">
+              <span class="letter">{{ letter }}</span>
+              <ProjectCard v-for = "project of Projects" v-show="project.title[0] === letter" :id = "project.id" :title  = "project.title" :link = "'/projects/' + project.id"/>
+          </div>
+        </div>
+        <div class="proj-right">
+          <div v-for="letter of letters2" class="letter-block">
+              <span class="letter">{{ letter }}</span>
+              <ProjectCard v-for = "project of Projects" v-show="project.title[0] === letter" :id = "project.id" :title  = "project.title" :link = "'/projects/' + project.id"/>
+          </div>
+        </div>
       </div>
+
     </main>
   </template>
   
   <script setup>
     const { data: Projects } = await useFetch('/api/projects')
+
+    // divide initial letters to display projects in two columns
+    const letters2 = ref([])
+    const letters1 = ref([])
+
+    letters2.value = Projects.value.map(project => project.title[0]).filter((value, index, self) => self.indexOf(value) === index)
+    letters1.value = letters2.value.splice(0, letters2.value.length/2)
+
   </script>
   
   <style>
-    #list-container
-    {
-      padding: 30px;
+    .alphabetical-projects {
       display: flex;
-      flex-wrap: wrap;
-      flex-direction: row;
-      justify-content:center;
-      gap: 50px;
+      justify-content: space-around;
     }
-  
-    main
-    {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-content: flex-start;
-      gap: 10px;
+
+    .letter-block {
+      margin: 60px;
     }
-    
+
+    .letter {
+      font-size: 40px;
+      font-weight: bold;
+      margin-bottom: 10px;
+      margin-left: 2%;
+    }
+
   </style>
