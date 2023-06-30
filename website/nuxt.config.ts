03f6f43343d5a1603817c44e660b90d74d8150e6
FrancescaGrimaldi <francesca1.grimaldi@mail.polimi.it>
2023-06-30 18:58:51 +0200

    maybe fixed og:image

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -25,7 +25,7 @@ export default defineNuxtConfig({
             meta: [
                 { charset: 'utf-8' },
                 { name: 'description', content: 'Wizarding Ventures website, where you can find all the information you need about us: projects, investments, areas of investment, contacts.' },
-                { name: 'og:image', content: 'https://i.imgur.com/HryjXvt' }, /* check if it works */
+                { name: 'og:image', content: 'https://i.imgur.com/HryjXvt.png' },
                 { name: 'author', content: 'Web Wizards' },
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' },
             ],
